c7e14fa55550b867454f5892ab052af97157991f
chris-tech-rgb <fatdasdavidson755@gmail.com>
2021-08-31 21:24:29 +0800

    event log 2

--- src/com/company/Server.java
+++ src/com/company/Server.java
@@ -134,6 +134,6 @@ public class Server {
     static void event(String message) {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
         LocalDateTime now = LocalDateTime.now();
-        System.out.println(dtf.format(now) + message);
+        System.out.println("[" + dtf.format(now) + "] " + message);
     }
 }
